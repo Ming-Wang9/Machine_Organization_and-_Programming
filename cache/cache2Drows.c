1202aaa64dd1a43c6c6ec1be8430a8427ebc9471
Ming Wang <132119331+Ming-Wang9@users.noreply.github.com>
2025-01-09 02:54:13 +0000

    1D and 2D cache

---
+++ cache/cache2Drows.c
@@ -0,0 +1,16 @@
+/*
+ * Name: Ming Wang
+ * Grade Group: g11
+ */
+
+int arr2D[3000][500];
+
+int main() {
+	for (int row = 0; row < 3000; row++){
+		for ( int col = 0; col < 500; col++){
+			arr2D[row][col] = row + col;
+		}
+	}
+	return 0;
+}
+		
